de8a5644386d08d11068428744bd5dc2eb42ada5
jieun Lee <devxjenn@gmail.com>
2022-03-26 01:39:29 +0900

    Feat: 할인정책 도메인 개발

---
+++ src/main/java/hello/core/discount/FixDiscountPolicy.java
@@ -0,0 +1,18 @@
+package hello.core.discount;
+
+import hello.core.member.Grade;
+import hello.core.member.Member;
+
+public class FixDiscountPolicy implements DiscountPolicy {
+    private int discountFixAmount = 1000; //1000원 할인
+
+    @Override
+    public int discount(Member member, int price) {
+        if(member.getGrade() == Grade.VIP){
+            // 회원 등급이 VIP인 경우, 1000원 할인
+            return discountFixAmount;
+        } else {
+            return 0;
+        }
+    }
+}
