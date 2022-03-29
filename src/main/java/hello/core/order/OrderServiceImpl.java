e2ca3cd39a98e02c242398809c431bfbd9e82760
jieun Lee <devxjenn@gmail.com>
2022-03-30 01:21:32 +0900

    Feat: 관심사의 분리, 생성자 의존관계 주입

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -9,10 +9,16 @@ import hello.core.member.MemoryMemberRepository;
 
 public class OrderServiceImpl implements OrderService{
 
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
 //    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();  // 고정할인, 문제점: Impl이 Fix구현클래스에 의존 -> DIP위반
 //    private final DiscountPolicy discountPolicy = new RateDiscountPolicy(); // 새로운 할인정책 적용, 문제점: Impl도 수정해야 함 -> OCP위반
-    private DiscountPolicy discountPolicy;  // DIP를 지키기 위해 인터페이스에만 의존하도록 설계 변경, 문제점: nullPointException 발생
+//    private DiscountPolicy discountPolicy;  // DIP를 지키기 위해 인터페이스에만 의존하도록 설계 변경, 문제점: nullPointException 발생, AppConfig 필요
+    private final MemberRepository memberRepository;
+    private final DiscountPolicy discountPolicy;
+
+    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }
 
     /*
     * 클라이언트로부터 주문 요청이 오면
