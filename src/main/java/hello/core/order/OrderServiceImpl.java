5f59f4bad2221a86a2c37d4ceaf1470661945807
jieun Lee <devxjenn@gmail.com>
2022-03-29 23:54:39 +0900

    Feat: 새로운 할인 정책 적용

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -2,6 +2,7 @@ package hello.core.order;
 
 import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
@@ -9,7 +10,9 @@ import hello.core.member.MemoryMemberRepository;
 public class OrderServiceImpl implements OrderService{
 
     private final MemberRepository memberRepository = new MemoryMemberRepository();
-    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+//    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();  // 고정할인, 문제점: Impl이 Fix구현클래스에 의존 -> DIP위반
+//    private final DiscountPolicy discountPolicy = new RateDiscountPolicy(); // 새로운 할인정책 적용, 문제점: Impl도 수정해야 함 -> OCP위반
+    private DiscountPolicy discountPolicy;  // DIP를 지키기 위해 인터페이스에만 의존하도록 설계 변경, 문제점: nullPointException 발생
 
     /*
     * 클라이언트로부터 주문 요청이 오면
