a21cb1601dba413e1153210d1e32fbff2411a3cc
jieun Lee <devxjenn@gmail.com>
2022-03-30 16:47:38 +0900

    Feat: parameter name 수정

--- src/main/java/hello/core/order/OrderService.java
+++ src/main/java/hello/core/order/OrderService.java
@@ -2,6 +2,6 @@ package hello.core.order;
 
 public interface OrderService {
     // return 으로 주문 결과(Order)를 반환한다.
-    Order createOrder(Long memberId, String item, int itemPrice);
+    Order createOrder(Long memberId, String itemName, int itemPrice);
 
 }
