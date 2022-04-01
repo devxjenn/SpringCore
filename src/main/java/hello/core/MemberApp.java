72b0b9259db3b50d5f3a46ca0f6931e58879f108
jieun Lee <devxjenn@gmail.com>
2022-04-02 00:04:49 +0900

    Feat: MemberApp에 스프링 컨테이너 적용 수정

--- src/main/java/hello/core/MemberApp.java
+++ src/main/java/hello/core/MemberApp.java
@@ -3,7 +3,6 @@ package hello.core;
 import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
-import hello.core.member.MemberServiceImpl;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
@@ -14,7 +13,7 @@ public class MemberApp {
 
         //스프링 컨테이너 생성 : 객체들을 모두 관리해줌 (ex. @Bean ...)
         ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
-        ApplicationContext.getBean("memberService", MemberService.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
 
         Member member = new Member(1L, "memberA", Grade.VIP);
         memberService.join(member);
